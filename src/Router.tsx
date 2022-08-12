049913317b5704426a3dd383bef7c107eb8dc755
Real-Bird <marbil815@gmail.com>
2022-08-12 20:46:37 +0900

    22-08-12 deploy try 1

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ function Router({ isLight }: { isLight: boolean }) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Coins />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Coins />} />
         <Route path=":coinId/*" element={<Coin isLight={isLight} />} />
       </Routes>
     </BrowserRouter>
